b6b5ed364193c73692702a2f3f1926097be22d34
hsd3211 <halsteadb3211@masseyhigh.school.nz>
2024-06-24 09:47:05 +1200

    variables testing program

---
+++ P_02_variables.py
@@ -0,0 +1,22 @@
+# Code for learning about variables....
+
+# Variables can be a number
+
+fav_num = 7
+num_string = "5"
+
+# Variables can be boolean (true/false)
+
+love_programming = True
+
+# Constants are uppercase (they don't change)
+PI = 3.14
+
+print(fav_num * 2)
+print(num_string * 5)
+print(love_programming)
+
+# This variable has a problem!
+my_name = "Miss G"
+
+
